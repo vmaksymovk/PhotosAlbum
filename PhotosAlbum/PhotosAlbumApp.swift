fc288c73d8b407f367a06a328c92bf705ec0e77f
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-12-13 13:40:25 +0100

    first commit

---
+++ PhotosAlbum/PhotosAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosAlbumApp.swift
+//  PhotosAlbum
+//
+//  Created by Влад on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotosAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
